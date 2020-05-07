14a8896bd8f5d2ce5ffe7ff4b47ad11d5f2d5bf6
Basilio Gómez Navarro <alu0101049151@ull.edu.es>
2020-05-07 12:34:48 +0100

    Innitial Commit

---
+++ tokenizator.js
@@ -0,0 +1,23 @@
+/**
+ * @author: Basilio Gómez Navarro
+ * @since: 07/05/2020
+ * Universidad de La Laguna
+ * Subject: Advanced Artificial Intelligence
+ * Escuela Superior de Ingeniería y Tecnología
+ * Computer Ingenieering Degree
+ * Grade: 3rd
+ * Project: Classification of Texts in Natural Language.
+ * Email: alu0101049151@ull.edu.es
+ * @desc: tokenizator.js file: Contains an implementation of a lexical analyzer 
+ *        that, from an input file with data, generates a dictionary with.
+ *
+ * References:
+ *
+ * Assignment statement:
+ * @link https://campusvirtual.ull.es/ingenieriaytecnologia/mod/resource/view.php?id=248911
+ *
+ * Version History
+ *                   07/05/2020 - Creation (first version) of the code.
+ */
+'use strict';
+
